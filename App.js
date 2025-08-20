f2c9ffe2bf37a903681bb3ce9fd1ea551a0e9111
RobsonGFerrarezi <g.robsonferrarezi@gmail.com>
2025-08-19 22:32:32 -0300

    Calculadora Funcionando todas as contas, mas ainda preciso arrumar visualização

--- App.js
+++ App.js
@@ -1,20 +1,159 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Alert, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
+
+  const [valor1, setValor1] = useState(0);
+  const [valor2, setValor2] = useState(0);
+  const [resultado, setResultado] = useState(0);
+  const [operacao, setOperacao] = useState('')
+
+  function calcular() {
+    if (valor1 === '' || valor2 === '') {
+      Alert.alert('ERRO:', 'Por favor, preencha todos os campos!');
+      return;
+    }
+
+    const v1 = Number.parseFloat(valor1.replace(',', '.'));
+    const v2 = Number.parseFloat(valor2.replace(',', '.'));
+
+    switch (operacao) {
+      case 'somar':
+        const soma = v1 + v2;
+        setResultado(soma);
+        Alert.alert('Resultado:', `A soma: ${valor1} + ${valor2} = ${soma}`);
+        break;
+      case 'subtrair':
+        const sub = v1 - v2;
+        setResultado(sub);
+        Alert.alert('Resultado:', `A subtração: ${valor1} - ${valor2} = ${sub}`);
+        break;
+      case 'multiplicar':
+        const prod = v1 * v2;
+        setResultado(prod);
+        Alert.alert('Resultado:', `O produto: ${valor1} x ${valor2} = ${prod}`);
+        break;
+      case 'dividir':
+        if (v2 === 0) {
+          Alert.alert('ERRO:', 'O segundo valor não pode ser 0 para uma divisão!');
+          return;
+        }
+        const div = v1 / v2;
+        setResultado(div);
+        Alert.alert('Resultado:', `A divisão: ${valor1} / ${valor2} = ${div}`);
+        break;
+      case 'exponencial':
+        const expo = v1 ** v2;
+        setResultado(expo);
+        Alert.alert('Resultado:', `A exponencial: ${valor1} ^ ${valor2} = ${expo}`);
+        break;
+      default:
+        Alert.alert('ERRO:', 'Selecione uma operação!');
+    }
+    
+  }
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.labelCampo}>Digite o primeiro valor: </Text>
+      <TextInput style={styles.caixaTexto}
+        keyboardType="decimal-pad"
+        onChangeText={(text) => setValor1(text)}
+      />
+
+      <Text style={styles.labelCampo}>Digite o segundo valor: </Text>
+      <TextInput style={styles.caixaTexto}
+        keyboardType="decimal-pad"
+        onChangeText={(text) => setValor2(text)}
+      />
+
+
+      {/* Caixa de seleção de operação */}
+      <View style={styles.op}>
+        <TouchableOpacity onPress={() => setOperacao('somar')} style={styles.botao}>
+          <Text style={styles.botaoTexto}>Somar</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => setOperacao('subtrair')} style={styles.botao}>
+          <Text style={styles.botaoTexto}>Subtrair</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => setOperacao('multiplicar')} style={styles.botao}>
+          <Text style={styles.botaoTexto}>Multiplicar</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => setOperacao('dividir')} style={styles.botao}>
+          <Text style={styles.botaoTexto}>Divisão</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => setOperacao('exponencial')} style={styles.botao}>
+          <Text style={styles.botaoTexto}>Exponenciação</Text>
+        </TouchableOpacity>
+      </View>
+
+
+      <TouchableOpacity style={styles.botao}
+        onPress={() => calcular()}
+      >
+        <Text style={styles.botaoTexto}>Calcular</Text>
+      </TouchableOpacity>
+
+      <Text style={styles.labelCampo}>Resultado: {resultado}</Text>
+
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  op: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#515050',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  titulo: {
+    fontSize: 30,
+    fontFamily: 'Courier New',
+    color: '#f000ff',
+    marginBottom: 15,
+    borderColor: '#fff',
+    borderWidth: 2,
+    padding: 5,
+    borderRadius: 10,
+  },
+  caixaTexto: {
+    borderBottomColor: '#000',
+    borderWidth: 1,
+    borderBottomWidth: 3,
+    width: '57%',
+    marginBottom: 15,
+    padding: 5,
+    fontSize: 20,
+    backgroundColor: '#aaaaaa',
+    color: '#fff',
+    borderRadius: 5,
+    textAlign: 'center',
+  },
+  botao: {
+    backgroundColor: '#f000ff',
+    padding: 10,
+    borderRadius: 15,
+    marginTop: 10,
+    width: '40%',
+  },
+  botaoTexto: {
+    color: '#fff',
+    fontSize: 20,
+    textAlign: 'center',
+  },
+  labelCampo: {
+    color: '#fff',
+    fontSize: 20,
+    marginBottom: 10,
+    paddingTop: 10,
   },
 });
